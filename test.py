9d1badea73f914f031af13314e236e42774adc70
jameson512 <2867557054@qq.com>
2023-11-19 18:10:26 +0800

    up

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ try:
     # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
     # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
     # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-    cred = credential.Credential("AKIDeFqaIrIXcWO7oIg4gCjPzNdltlcNQNJ5", "jMuOm1BEMeqvyvmbSEufNuPuJcyHavAJ")
+    cred = credential.Credential("", "")
     # 实例化一个http选项，可选的，没有特殊需求可以跳过
     httpProfile = HttpProfile()
     httpProfile.endpoint = "tmt.tencentcloudapi.com"
