5f355ff2eb21895e165b45103641deb3985c1e69
jameson512 <2867557054@qq.com>
2023-11-19 18:05:20 +0800

    up

---
+++ videotrans/translator/tencent.py
@@ -0,0 +1,43 @@
+import json
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.tmt.v20180321 import tmt_client, models
+from ..configure import config
+from ..configure.config import logger
+from ..configure  import tools as sptools
+
+def tencenttrans(text, src, dest):
+    try:
+        # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
+        # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
+        # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
+        cred = credential.Credential(config.video['tencent_SecretId'], config.video['tencent_SecretKey'])
+        # 实例化一个http选项，可选的，没有特殊需求可以跳过
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "tmt.tencentcloudapi.com"
+
+        # 实例化一个client选项，可选的，没有特殊需求可以跳过
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        # 实例化要请求产品的client对象,clientProfile是可选的
+        client = tmt_client.TmtClient(cred, "ap-beijing", clientProfile)
+
+        # 实例化一个请求对象,每个接口都会对应一个request对象
+        req = models.TextTranslateRequest()
+        params = {
+            "SourceText": text,
+            "Source": "auto",
+            "Target": dest,
+            "ProjectId": 0
+        }
+        req.from_json_string(json.dumps(params))
+
+        # 返回的resp是一个TextTranslateResponse的实例，与请求对象对应
+        resp = client.TextTranslate(req)
+        return resp.TargetText
+    except Exception as e:
+        logger.error("tencent api error:" + str(e))
+        sptools.set_process("[error]腾讯翻译失败:" + str(e))
+        return "tencent api error:" + str(e)
