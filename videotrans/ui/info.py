3c756ca38e3c195332b2dcc991b4ee36e74dc5ac
jameson512 <2867557054@qq.com>
2024-08-27 23:53:07 +0800

    Fix: del unused images

--- videotrans/ui/info.py
+++ videotrans/ui/info.py
@@ -11,7 +11,8 @@
 from PySide6 import QtCore, QtWidgets
 from PySide6.QtGui import QDesktopServices
 
-from ..configure import config
+from videotrans.configure import config
+from . import wx,alipay,mp
 
 
 class Ui_infoform(object):
